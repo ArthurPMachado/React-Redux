2305097e029e76729f74ae962da11acfb6788c65
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-12-11 11:40:00 -0300

    Feat: connect redux to Media component

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux'; 
+
 import Card from './Card';
 
 function Media(props) {
+  const { min, max } = props;
 
   return (
     <Card title="Média dos Números" green>
       <div>
         <span>
           <span>Resultado: </span>
-          <strong>5</strong>
+          <strong>{(max + min) / 2}</strong>
         </span>
       </div>
     </Card>
   )
 }
 
-export default Media;
+function mapStateToProps(state) {
+  const { min, max } = state.numeros;
+
+  return { min, max }
+}
+
+export default connect(mapStateToProps)(Media);
